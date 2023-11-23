0a8529e1fc80aa6ad56ea9ce4523dc104595d66b
Vladimir <w112vovan@mail.ru>
2023-11-23 14:40:53 +0500

    Добавьте файлы проекта.

---
+++ L11_sumOfNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L11_sumOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int maxNumber = 101;
+            int minNumber = 0;
+            int multiply1 = 3;
+            int multiply2 = 5;
+            int sumOfNumber = 0;
+            int randomNumber = random.Next(minNumber, maxNumber);
+
+            for (int i = minNumber; i <= randomNumber; i++)
+            {
+                if ((i % multiply1) == 0 || (i % multiply2) == 0)
+                    sumOfNumber += i;
+            }
+
+            Console.WriteLine($"Сумма всех чисел от {minNumber} до {randomNumber} включительно," +
+                              $" кратных {multiply1} или {multiply2}, равна {sumOfNumber}");
+        }
+    }
+}
