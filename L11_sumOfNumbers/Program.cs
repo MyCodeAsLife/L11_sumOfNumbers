7430cce7aa4fd68032bad88bd0ba113596a7e6db
Vladimir <w112vovan@mail.ru>
2023-11-26 02:52:09 +0500

    Правка 1

--- L11_sumOfNumbers/Program.cs
+++ L11_sumOfNumbers/Program.cs
@@ -15,17 +15,17 @@ namespace L11_sumOfNumbers
             int minNumber = 0;
             int multiply1 = 3;
             int multiply2 = 5;
-            int sumOfNumber = 0;
+            int sumOfNumbers = 0;
             int randomNumber = random.Next(minNumber, maxNumber);
 
             for (int i = minNumber; i <= randomNumber; i++)
             {
                 if ((i % multiply1) == 0 || (i % multiply2) == 0)
-                    sumOfNumber += i;
+                    sumOfNumbers += i;
             }
 
             Console.WriteLine($"Сумма всех чисел от {minNumber} до {randomNumber} включительно," +
-                              $" кратных {multiply1} или {multiply2}, равна {sumOfNumber}");
+                              $" кратных {multiply1} или {multiply2}, равна {sumOfNumbers}");
         }
     }
 }
